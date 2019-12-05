c6e96623bb8135f58552eb68486de5afab63622f
Nobody <efra123d456@gmail.com>
2019-12-05 16:40:02 -0400

    api

--- ApiRest/routes/service.js
+++ ApiRest/routes/service.js
@@ -1,6 +1,7 @@
 var express = require("express");
 
 var USER = require("../database/users");
+var HOME = require("../database/homes");
 //const USER = user.model;
 //const USERSCHEMA = user.schema;
 
@@ -17,7 +18,7 @@ router.post('/user', async(req, res) => {
 });
 
 /*Show Users*/
-router.get("/user", (req, res) => {
+router.get('/user', (req, res) => {
     var params = req.query;
     var limit = 100;
     if (params.limit != null) {
@@ -30,5 +31,38 @@ router.get("/user", (req, res) => {
     USER.find({}).limit(limit).skip(skip).exec((err, docs) => {
         res.status(200).json(docs);
     });
+});
+
+/*Creation Homes*/
+router.post('/home', async(req, res)=>{
+    console.log(req.body);
+    var params = req.body;
+    var homes = new HOME(params);
+    var result = await homes.save();
+    res.status(200).json(result);
+})
+
+/**Show Homes */
+router.get('/home', (req, res) => {
+
+});
+
+/**Creation Neighborhood */
+router.post('/neighborhood', async(req, res) => {
+    console.log(req.body);
+    var params = req.body;
+    var neighborhoods = new NEIGHBORHOOD(params);
+    var result = await neighborhoods.save();
+    res.status(200).json(result);
+});
+
+/**Show Neighborhood */
+router.get('/neighborhood', (req, res) => {
+    
+});
+
+/**LOGIN */
+router.post('/login', async(req, res, next)=>{
+
 });
 module.exports = router;

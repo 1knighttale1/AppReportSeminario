ffc93f04a3bf8ccbe935f6889947123db6d7b34f
1knighttale1 <1knighttale1@gmail.com>
2019-12-06 00:31:04 -0400

    agregando servicios

--- ApiRest/routes/service.js
+++ ApiRest/routes/service.js
@@ -2,9 +2,7 @@ var express = require("express");
 
 var USER = require("../database/users");
 var HOME = require("../database/homes");
-var NEIGHBORHOOD = require("../database/zones");
-//const USER = user.model;
-//const USERSCHEMA = user.schema;
+var ZONE = require("../database/zones");
 var router = express.Router();
 var jwt = require("jsonwebtoken");
 
@@ -18,6 +16,26 @@ router.post('/user', async(req, res) => {
     var result = await users.save();
     res.status(200).json(result);
 });
+
+/**Creation Zones */
+router.post('/zone', async(req, res) => {
+    console.log(req.body);
+    var params = req.body;
+    var zone = new ZONE(params);
+    var result = await zone.save();
+    res.status(200).json(result);
+});
+
+/*Creation Homes*/
+router.post('/home', async(req, res)=>{
+    console.log(req.body);
+    var params = req.body;
+    var homes = new HOME(params);
+    var result = await homes.save();
+    res.status(200).json(result);
+})
+
+/** ------------------------------------------------ */
 
 /*Show Users*/
 router.get('/user', (req, res) => {
@@ -35,33 +53,85 @@ router.get('/user', (req, res) => {
     });
 });
 
-/*Creation Homes*/
-router.post('/home', async(req, res)=>{
-    console.log(req.body);
-    var params = req.body;
-    var homes = new HOME(params);
-    var result = await homes.save();
-    res.status(200).json(result);
-})
-
-/**Show Homes */
+/*Looking a specific home*/
 router.get('/home', (req, res) => {
+    
+    var params = req.query;
+    var filter = {};
 
+    if(params.id != null){
+        filter["id"] = Number(params.id);
+        HOME.find(filter).exec((err, docs) => {
+            if (err){
+                res.status(300).json({
+                    msn:"Error en la base de datos"
+                });
+                return;
+            }
+            res.status(200).json(docs);
+            console.log("Looking a specific home");
+            return;
+        });
+    }else{
+        res.status(200).json({
+            msn: "No se encontro la casa"
+        });
+    }
 });
 
-/**Creation Neighborhood */
-router.post('/neighborhood', async(req, res) => {
-    console.log(req.body);
-    var params = req.body;
-    var neighborhoods = new NEIGHBORHOOD(params);
-    var result = await neighborhoods.save();
-    res.status(200).json(result);
+/**Show Homes */
+router.get('/homes', (req, res) => {
+    var cad = "contacto lat long precio imagen"; //detalles previos
+    var skip = 0;
+    var limit = 20;
+    var filter = {};
+    var params = req.query;
+
+    if(params.skip != null){
+        skip = Number(params.skip);
+    }
+    if(params.limit != null){
+        limit = Number(params.limit);
+    }
+    //busqueda por direccion
+    if(params.search != null){
+        filter["directions"] = new RegExp(params.search, "g");
+    }
+    //busqueda por precio
+    if(params.min != null && params.max){
+        filter["precio"] = {"$gt": Number(params.min), "$lt": Number(params.max)};
+    }
+    
+    HOME.find(filter).skip(skip).limit(limit).select(cad).exec((err, docs) => {
+        if (err){
+            res.status(300).json({
+                msn:"Error en la base de datos"
+            });
+            return;
+        }
+        res.status(200).json(docs);
+        console.log("Looking homes");
+        return;
+    });
 });
 
 /**Show Neighborhood */
-router.get('/neighborhood', (req, res) => {
-    
+router.get('/zone', (req, res) => {
+    var params = req.query;
+    var limit = 100;
+    if (params.limit != null) {
+        limit = parseInt(params.limit);
+    } 
+    var skip = 0;
+    if (params.skip != null) {
+        skip = parseInt(params.skip);
+    }
+    ZONE.find({}).limit(limit).skip(skip).exec((err, docs) => {
+        res.status(200).json(docs);
+    });
 });
+
+/** ------------------------------------------------------------------------------ */
 
 /**LOGIN */
 router.post('/login', (req, res, next)=>{

39a9f57db87c6efcd61c546f4bdd397ac78f2b55
JRuddyC <ruddyxd007@gmail.com>
2019-12-08 12:49:22 -0400

    mejora registro

--- AndroidGG/Login/app/src/main/java/com/example/log_in/RegisterActivity.java
+++ AndroidGG/Login/app/src/main/java/com/example/log_in/RegisterActivity.java
@@ -5,16 +5,49 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Spinner sexo = findViewById(R.id.spnSexo);
+        Spinner tp_user = findViewById(R.id.spnTp_user);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.txt_sexo, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sexo.setAdapter(adapter);
+        sexo.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.txt_tp_user, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tp_user.setAdapter(adapter1);
+        tp_user.setOnItemSelectedListener(this);
     }
+
+
     @Override
     public void onClick(View view){
         finish();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        //Toast.makeText(parent.getContext(), text , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }

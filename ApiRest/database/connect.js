15ad2eaa85b5363ec42e9656491f29ec626ca7fd
1knighttale1 <1knighttale1@gmail.com>
2019-12-14 18:26:42 -0400

    connect

--- ApiRest/database/connect.js
+++ ApiRest/database/connect.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
 const databasename = "apirest";
-mongoose.connect("mongodb://172.22.0.2:27017/" + databasename);
+mongoose.connect("mongodb://172.21.0.2:27017/" + databasename);
+//mongoose.connect("mongodb://172.22.0.2:27017/" + databasename);
 module.exports = mongoose;
